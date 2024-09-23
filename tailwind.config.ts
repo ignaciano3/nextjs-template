fe8cd480ae5d1151572ca43d6c689f3458c64d40
Ignacio Garcia Pizales <igarciap@fi.uba.ar>
2024-09-22 22:58:42 -0300

    actualizo tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,21 +2,12 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{ts,tsx,mdx}",
+    "./src/app/**/*.{ts,tsx,mdx}",
+    "./src/features/**/*.{ts,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-      fontFamily: {
-        sans: ["var(--font-geist-sans)"],
-        mono: ["var(--font-geist-mono)"],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
